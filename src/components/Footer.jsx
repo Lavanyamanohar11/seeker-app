f406a4130d5f1fd4daa6451e5e564f1cb57dd657
Lavanya M <lavanyamanohar11@gmail.com>
2021-11-01 18:40:13 +0530

    final

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer = () => (
+  <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+    Made with 🤍 by <a href="https://github.com/Lavanyamanohar11" target="_blank">Lavanya</a> &copy; 2021
+  </div>
+);
